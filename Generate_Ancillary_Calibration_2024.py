04abad3ab90248f29035cc2b76a9f1c3cf5ed9be
Verreyken Bert <bert.verreyken@uliege.ne>
2024-11-18 13:32:34 +0100

    Add calibration mixture of 2024 and include uncertainty in calibration mixture.

--- Generate_Ancillary_Calibration_2024.py
+++ Generate_Ancillary_Calibration_2024.py
@@ -27,8 +27,8 @@ Tr_selection[mz] = False
 
 # Protonated Methanol
 mz = 33.033
-mixrat_bottle[mz] = 1030
-unc_mixrat_bottle[mz] = 48/2 # coverage factor 2 and stated uncertainty 48
+mixrat_bottle[mz] = 1068
+unc_mixrat_bottle[mz] = 70/2 # coverage factor 2 and stated uncertainty 48
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2.20, 2.20, 2.20
 fr[mz] = 1.014
 Tr_selection[mz] = True
@@ -58,23 +58,23 @@ Tr_selection[mz] = False
 
 # Protonated Acetonitrile
 mz = 42.034
-mixrat_bottle[mz] = 537
-unc_mixrat_bottle[mz] = 20/2
+mixrat_bottle[mz] = 490
+unc_mixrat_bottle[mz] = 23/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 3.10, 4.40, 3.10
 fr[mz] = 1.026
 Tr_selection[mz] = True
 
 # Protonated Acetaldehyde
 mz = 45.033
-mixrat_bottle[mz] = 991
-unc_mixrat_bottle[mz] = 32/2
+mixrat_bottle[mz] = 1006
+unc_mixrat_bottle[mz] = 36/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 3.03, 3.03, 3.03
 fr[mz] = 1.025
 Tr_selection[mz] = True
 
 # Protonated Ethanol
 mz = 47.049
-mixrat_bottle[mz] = 537
+mixrat_bottle[mz] = 493
 unc_mixrat_bottle[mz] = 20/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 3.00, 3.00, 3.00
 fr[mz] = 1
@@ -82,32 +82,32 @@ Tr_selection[mz] = False
 
 # Protonated Acetone
 mz = 59.049
-mixrat_bottle[mz] = 1006
-unc_mixrat_bottle[mz] = 36/2
+mixrat_bottle[mz] = 1026
+unc_mixrat_bottle[mz] = 33/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 3.25, 3.70, 3.25
 fr[mz] = 1.036
 Tr_selection[mz] = True
 
 # Protonated Isoprene
 mz = 69.070
-mixrat_bottle[mz] = 499
-unc_mixrat_bottle[mz] = 16/2
+mixrat_bottle[mz] = 508
+unc_mixrat_bottle[mz] = 15/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 1.85, 1.85, 1.85
 fr[mz] = 1.057
 Tr_selection[mz] = False
 
 # Protonated Isoprene oxidation product (50/50 MACR/MVK)
 mz = 71.049
-mixrat_bottle[mz] = 477+514
-unc_mixrat_bottle[mz] = ((16/2)**2+(17/2)**2)**.5
+mixrat_bottle[mz] = 470+501
+unc_mixrat_bottle[mz] = ((14/2)**2+(15/2)**2)**.5
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2.72, 2.72, 2.72
 fr[mz] = 1.048
 Tr_selection[mz] = False
 
 # Protonated MEK
 mz = 73.065
-mixrat_bottle[mz] = 520
-unc_mixrat_bottle[mz] = 17/2
+mixrat_bottle[mz] = 502
+unc_mixrat_bottle[mz] = 19/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 3.25, 3.25, 3.25
 fr[mz] = 1.048
 Tr_selection[mz] = False
@@ -122,23 +122,23 @@ Tr_selection[mz] = False
 
 # Protonated Benzene
 mz = 79.054
-mixrat_bottle[mz] = 514
-unc_mixrat_bottle[mz] = 17/2
+mixrat_bottle[mz] = 506
+unc_mixrat_bottle[mz] = 15/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 1.97, 2.00, 1.97
 fr[mz] = 1.068
 Tr_selection[mz] = True
 
 # Protonated monoterpene fragment (100% sabinene)
 mz = 81.070
-mixrat_bottle[mz] = 976
-unc_mixrat_bottle[mz] = 36/2
+mixrat_bottle[mz] = 982
+unc_mixrat_bottle[mz] = 30/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2.04, 2.04, 2.04
 fr[mz] = 1.068
 Tr_selection[mz] = False
 
 # Protonated dehydrated cis-3-hexenol
 mz = 83.086
-mixrat_bottle[mz] = 948
+mixrat_bottle[mz] = 986
 unc_mixrat_bottle[mz] = 31/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2, 2, 2
 fr[mz] = 1.069
@@ -146,15 +146,15 @@ Tr_selection[mz] = False
 
 # Protonated Toluene
 mz = 93.070
-mixrat_bottle[mz] = 494
-unc_mixrat_bottle[mz] = 16/2
+mixrat_bottle[mz] = 491
+unc_mixrat_bottle[mz] = 15/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2.15, 2.12, 2.12
 fr[mz] = 1.080
 Tr_selection[mz] = True
 
 # Protonated cis-3-hexenol
 mz = 101.096
-mixrat_bottle[mz] = 948
+mixrat_bottle[mz] = 986
 unc_mixrat_bottle[mz] = 31/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2, 2, 2
 fr[mz] = 1.069
@@ -163,32 +163,32 @@ Tr_selection[mz] = False
 
 # Protonated m-xylene
 mz = 107.086
-mixrat_bottle[mz] = 492
-unc_mixrat_bottle[mz] = 16/2
+mixrat_bottle[mz] = 487
+unc_mixrat_bottle[mz] = 15/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2.31, 2.26, 2.31
 fr[mz] = 1.092
 Tr_selection[mz] = True
 
 # Protonated 1,2,4-trimethylbenzene
 mz = 121.101
-mixrat_bottle[mz] = 479
-unc_mixrat_bottle[mz] = 19/2
+mixrat_bottle[mz] = 482
+unc_mixrat_bottle[mz] = 17/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2.40, 2.20, 2.40
 fr[mz] = 1.105
 Tr_selection[mz] = False
 
 # Protonated 1,2,3-trifluorobenzene
 mz = 133.026
-mixrat_bottle[mz] = 536
-unc_mixrat_bottle[mz] = 27/2
+mixrat_bottle[mz] = 531
+unc_mixrat_bottle[mz] = 22/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2.46, 2.46, 2.46
 fr[mz] = 1.067
 Tr_selection[mz] = False
 
 # Protonated monoterpene (100% sabinene)
 mz = 137.132
-mixrat_bottle[mz] = 976
-unc_mixrat_bottle[mz] = 36/2
+mixrat_bottle[mz] = 982
+unc_mixrat_bottle[mz] = 31/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = 2.04, 2.04, 2.04
 fr[mz] = 1.117
 Tr_selection[mz] = False
@@ -219,16 +219,16 @@ Tr_selection[mz] = False
 
 # Protonated D4-Siloxane
 mz = 297.082
-mixrat_bottle[mz] = 987
-unc_mixrat_bottle[mz] = 37/2
+mixrat_bottle[mz] = 989
+unc_mixrat_bottle[mz] = 32/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = np.nan, 2.00, 2.99
 fr[mz] = 1.5
 Tr_selection[mz] = True
 
 # Protonated D5-Siloxane
 mz = 371.101
-mixrat_bottle[mz] = 979
-unc_mixrat_bottle[mz] = 48/2
+mixrat_bottle[mz] = 1008
+unc_mixrat_bottle[mz] = 31/2
 k[mz], k_ionicon[mz], k_Holzinger[mz] = np.nan, 2.00, 3.39
 fr[mz] = 1.7
 Tr_selection[mz] = True
@@ -244,4 +244,4 @@ Tr_selection[mz] = False
 calibration_ancillary = pd.DataFrame([mixrat_bottle,unc_mixrat_bottle,k,k_ionicon,k_Holzinger,fr, Tr_selection],index=['mixrat_bottle [ppbv]','1_sigm [ppbv]','k [1.e-9 cm3 molecule-1 s-1]','k_ionicon [1.e-9 cm3 molecule-1 s-1]','k_Holzinger [1.e-9 cm3 molecule-1 s-1]','fr','Tr_selection']).transpose()
 calibration_ancillary.index.name = 'mz_exact'
 
-calibration_ancillary.to_csv('./Calibration_Ancillary.csv'.format())
+calibration_ancillary.to_csv('./Calibration_Ancillary_2024.csv'.format())
